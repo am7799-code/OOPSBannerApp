45db9d74d841747ae0ceceee6f4e98be30f66e97
Expacio <akshay.mishra2228@gmail.com>
2026-02-23 01:29:04 +0530

    [Akshay] Printing a large ascii oopsgit add printlargeoops.java

---
+++ printlargeoops.java
@@ -0,0 +1,28 @@
+public class OOPSBannerUC2 {
+
+    public static void main(String[] args) {
+
+        String O1 = " ******** ";
+        String O2 = "*        *";
+        String O7 = " ******** ";
+
+        String P1 = "********  ";
+        String P2 = "*       * ";
+        String P4 = "********  ";
+        String P5 = "*         ";
+
+        String S1 = " ******** ";
+        String S2 = "*         ";
+        String S4 = " ******** ";
+        String S5 = "        * ";
+        String S7 = " ******** ";
+
+        System.out.println(O1 + " " + O1 + " " + P1 + " " + S1);
+        System.out.println(O2 + " " + O2 + " " + P2 + " " + S2);
+        System.out.println(O2 + " " + O2 + " " + P2 + " " + S2);
+        System.out.println(O2 + " " + O2 + " " + P4 + " " + S4);
+        System.out.println(O2 + " " + O2 + " " + P5 + " " + S5);
+        System.out.println(O2 + " " + O2 + " " + P5 + " " + S5);
+        System.out.println(O7 + " " + O7 + " " + P5 + " " + S7);
+    }
+}
