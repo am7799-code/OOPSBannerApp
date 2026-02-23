f64c59676ed7ac532421c9907bf9bc37a312af87
Expacio <akshay.mishra2228@gmail.com>
2026-02-23 08:46:20 +0530

    uc3 commit by akshay

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,49 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println("OOPS Banner App - UC3\n");
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  ****** ",
+                " ********"));
+
+        System.out.println(String.join(" ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                "**     **"));
+
+        System.out.println(String.join(" ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                "**     **"));
+
+        System.out.println(String.join(" ",
+                " **   ** ",
+                " **   ** ",
+                " ******  ",
+                " ******  "));
+
+        System.out.println(String.join(" ",
+                " **   ** ",
+                " **   ** ",
+                " **      ",
+                "      ** "));
+
+        System.out.println(String.join(" ",
+                " **   ** ",
+                " **   ** ",
+                " **      ",
+                "      ** "));
+
+        System.out.println(String.join(" ",
+                "  *****  ",
+                "  *****  ",
+                "  **     ",
+                " ******  "));
+    }
+}
